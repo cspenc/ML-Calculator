f012a643fc5deae94ac047fa42c411d957d32445
cspenc <crs992@gmail.com>
2017-05-09 19:37:31 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      digit: '0'
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="calculator">
+          <div className="display">0</div>
+          <div className="buttons">
+            <div className="button top">AC</div>
+            <div className="button top">+/-</div>
+            <div className="button top">%</div>
+            <div className="button right">div</div>
+
+            <div className="button classic">7</div>
+            <div className="button classic">8</div>
+            <div className="button classic">9</div>
+            <div className="button right">X</div>
+
+            <div className="button classic">4</div>
+            <div className="button classic">5</div>
+            <div className="button classic">6</div>
+            <div className="button right">-</div>
+
+            <div className="button classic">1</div>
+            <div className="button classic">2</div>
+            <div className="button classic">3</div>
+            <div className="button right">+</div>
+
+            <div className="button classic zero">0</div>
+            <div className="button classic">.</div>
+            <div className="button right">=</div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
+
 }
 
 export default App;
