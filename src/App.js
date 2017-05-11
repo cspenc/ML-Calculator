c1bddeda354365efea76c9e71173d5fa132b9b12
cspenc <crs992@gmail.com>
2017-05-11 15:35:18 -0400

    negative

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ class App extends Component {
     this.inputDigit = this.inputDigit.bind(this);
     this.clearDisplay = this.clearDisplay.bind(this);
     this.inputDot = this.inputDot.bind(this);
+    this.neg = this.neg.bind(this);
+    this.percent = this.percent.bind(this);
 
   }
 
   inputDigit(val) {
     this.setState({
-      digit: this.state.digit === '0' ? String(val) : this.state.digit + val
+      digit: this.state.digit === '0' ? val : this.state.digit + val
     })
   }
 
@@ -37,6 +39,18 @@ class App extends Component {
     }
   }
 
+  neg() {
+    this.setState({
+      digit: this.state.digit.charAt(0) === '-' ? this.state.digit.substr(1) : '-' + this.state.digit
+    })
+  }
+
+  percent() {
+    this.setState({
+      digit: this.state.digit
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -44,26 +58,26 @@ class App extends Component {
           <div className="display">{this.state.digit}</div>
           <div className="buttons">
             <div className="button top" onClick={this.clearDisplay}>AC</div>
-            <div className="button top">+/-</div>
-            <div className="button top">%</div>
-            <div className="button right">div</div>
+            <div className="button top" onClick={this.neg}>±</div>
+            <div className="button top" onClick={this.percent}>%</div>
+            <div className="button right">÷</div>
 
-            <div className="button classic" onClick={() => this.inputDigit(7)}>7</div>
-            <div className="button classic" onClick={() => this.inputDigit(8)}>8</div>
-            <div className="button classic" onClick={() => this.inputDigit(9)}>9</div>
+            <div className="button classic" onClick={() => this.inputDigit('7')}>7</div>
+            <div className="button classic" onClick={() => this.inputDigit('8')}>8</div>
+            <div className="button classic" onClick={() => this.inputDigit('9')}>9</div>
             <div className="button right">X</div>
 
-            <div className="button classic" onClick={() => this.inputDigit(4)}>4</div>
-            <div className="button classic" onClick={() => this.inputDigit(5)}>5</div>
-            <div className="button classic" onClick={() => this.inputDigit(6)}>6</div>
-            <div className="button right">-</div>
+            <div className="button classic" onClick={() => this.inputDigit('4')}>4</div>
+            <div className="button classic" onClick={() => this.inputDigit('5')}>5</div>
+            <div className="button classic" onClick={() => this.inputDigit('6')}>6</div>
+            <div className="button right">–</div>
 
-            <div className="button classic" onClick={() => this.inputDigit(1)}>1</div>
-            <div className="button classic" onClick={() => this.inputDigit(2)}>2</div>
-            <div className="button classic" onClick={() => this.inputDigit(3)}>3</div>
+            <div className="button classic" onClick={() => this.inputDigit('1')}>1</div>
+            <div className="button classic" onClick={() => this.inputDigit('2')}>2</div>
+            <div className="button classic" onClick={() => this.inputDigit('3')}>3</div>
             <div className="button right">+</div>
 
-            <div className="button classic zero" onClick={() => this.inputDigit(0)}>0</div>
+            <div className="button classic zero" onClick={() => this.inputDigit('0')}>0</div>
             <div className="button classic" onClick={this.inputDot}>.</div>
             <div className="button right">=</div>
           </div>
